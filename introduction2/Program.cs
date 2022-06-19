c6907c0e27e4815ef46e80e46e2fad1701fa0ca3
RamilBinnatov <ramilrb@code.edu.az>
2022-06-20 01:23:39 +0400

    c# algoritm

---
+++ introduction2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduction2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Verilmiş n -ədədinin 3 - ə və 7 - ə bölünüb - bölünməməsini tapın.
+            Console.Write("Enter a Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            if (n % 3 == 0 && n % 7 == 0)
+            {
+                Console.WriteLine("divisible;");
+            }
+            else
+
+            {
+                Console.WriteLine("indivisible");
+            }
+            */
+
+            /*2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+                Console.Write("Enter a Number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("one more:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            if (n % 2 == 0 && m % 2 == 0)
+            {
+                Console.WriteLine(n + m);
+            }
+            else
+            {
+                Console.WriteLine("not an even number");
+            }
+            */
+
+            //3.Verilmis n ve m(n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+            /*
+            int a = 1;
+            int b = 7;
+            int sum = 0;
+
+
+            for (int i = a; i < b; i++)
+            {
+                if (i % 2 == 1)
+                    sum += i;
+
+            }
+            Console.WriteLine(sum - a);
+            */
+
+            // 4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+            /*
+            int[] nums = { 1, 3, 4, 9, 5, 6, 7 };
+
+            int sum = 0;
+
+
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                if (nums[i] % 2 == 1)
+                {
+                    sum = sum + nums[i];
+                }
+
+            }
+
+            Console.WriteLine(sum);
+            */
+
+            //5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+            /*int[] nums = { 1, 2, 5, 6, 8, 11, 13, 12, 16, 26 };
+
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+            */
+
+
+
+        }
+    }
+}
